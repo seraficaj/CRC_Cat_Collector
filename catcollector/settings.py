383ccd1b493a3c2a0b0aad4db2435f04e4210b64
Jason Serafica <seraficaj@gmail.com>
2023-08-09 11:17:12 -0700

    add database environment variables to settings.py

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -81,7 +82,11 @@ WSGI_APPLICATION = 'catcollector.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'catcollector',
+         'NAME': os.environ['DATABASE_NAME'],
+         'USER': os.environ['DATABASE_USER'],
+         'PASSWORD': os.environ['DATABASE_PASSWORD'],
+         'HOST': os.environ['DATABASE_SERVER'],
+         'PORT': '5432',
     }
 }
 
